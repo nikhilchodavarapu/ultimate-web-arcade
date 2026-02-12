5191a5e5b3ca9c9cc0cd98172cb2fc32f995ea7e
Mohammed Haji Hussain <mohammedhajihussain@Mohammeds-MacBook-Pro.local>
2026-02-12 16:42:44 +0530

    Removes the database from git and adds the scores tables with foreign key constraints

--- src/initialize-db.js
+++ src/initialize-db.js
@@ -2,9 +2,22 @@ import { DatabaseSync } from "node:sqlite";
 
 export const initializeDB = () => {
   const db = new DatabaseSync("./database/user-details.db");
-  db.exec(`CREATE TABLE IF NOT EXISTS REGISTRATION (
-    username TEXT PRIMARY KEY,
-    password TEXT
-    )`);
+  db.exec(`
+    CREATE TABLE IF NOT EXISTS REGISTRATION (
+      username TEXT PRIMARY KEY,
+      password TEXT
+    );
+
+    CREATE TABLE IF NOT EXISTS SCORES (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      username TEXT NOT NULL,
+      score INTEGER,
+      stars INTEGER,
+      FOREIGN KEY (username) REFERENCES REGISTRATION(username)
+    );
+    `);
   return db;
 };
+
+
+initializeDB();
